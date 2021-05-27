a282fac63e19ab4068c0e868cf4cc7219bcf19ca
Laxya Pahuja <laxya.pahuja8@gmail.com>
2021-05-27 16:16:34 +0530

    / redirect

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/api', indexRouter)
+app.use('/', indexRouter)
 app.use('/user', userRouter)
 app.use('/auth', authRouter)
 app.use('/room', roomRouter)
